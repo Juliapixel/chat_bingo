e3d9459e9946b5a0be9c51c35e5d5b6f760df693
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-02-10 23:40:38 -0300

    add tokio and empty actix-web server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,4 @@
+use actix_web::middleware::Logger;
 use env_logger::Env;
 use log::info;
 
@@ -12,11 +13,17 @@ const DEFAULT_LEVEL: &'static str = {
     }
 };
 
-fn main() {
+#[tokio::main]
+async fn main() {
     env_logger::init_from_env(
         Env::new()
             .filter_or("BINGO_LOG", DEFAULT_LEVEL)
     );
 
     info!("Chat Bingo initiated pag");
+
+    actix_web::HttpServer::new(|| {
+        actix_web::App::new()
+            .wrap(Logger::default())
+    }).bind(("127.0.0.1", 8080)).unwrap().run().await.unwrap();
 }
