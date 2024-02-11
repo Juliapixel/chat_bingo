ec74042d77581a49129d35ce9c127a94a91cf69b
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-02-10 23:29:05 -0300

    change default logging level based on build profile

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,21 @@
 use env_logger::Env;
 use log::info;
 
+const DEFAULT_LEVEL: &'static str = {
+    #[cfg(debug_assertions)]
+    {
+        "DEBUG"
+    }
+    #[cfg(not(debug_assertions))]
+    {
+        "INFO"
+    }
+};
+
 fn main() {
     env_logger::init_from_env(
         Env::new()
-            .filter_or("BINGO_LOG", "INFO")
+            .filter_or("BINGO_LOG", DEFAULT_LEVEL)
     );
 
     info!("Chat Bingo initiated pag");
