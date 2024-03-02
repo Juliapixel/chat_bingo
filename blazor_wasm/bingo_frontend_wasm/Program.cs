902fa9c9e9357274f7aa788200ac9a2d9bd6df9e
Vin <104235331+VinFG1@users.noreply.github.com>
2024-03-01 21:33:22 -0800

    Add git hash embedding

--- blazor_wasm/bingo_frontend_wasm/Program.cs
+++ blazor_wasm/bingo_frontend_wasm/Program.cs
@@ -1,12 +1,28 @@
 using bingo_frontend_wasm;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+using System.Reflection;
 
 
-await builder.Build().RunAsync();
+class Program
+{
+    internal static string GITHASH { get; set; } = "";
+
+    static async Task Main(string[] args)
+    {
+        GITHASH = Assembly.GetEntryAssembly()
+                          .GetCustomAttributes<AssemblyMetadataAttribute>()
+                          .FirstOrDefault(attr => attr.Key == "GitHash")?.Value;
+
+        Console.WriteLine(GITHASH);
+
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+        await builder.Build().RunAsync();
+    }
+}
