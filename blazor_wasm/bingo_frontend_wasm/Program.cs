283c8c3f8766c5b09e116122dcf26088566e765e
Vin <104235331+VinFG1@users.noreply.github.com>
2024-03-04 16:30:53 -0800

    Add dev/prod environment flags, allowing certain elements to be toggled off in the future

--- blazor_wasm/bingo_frontend_wasm/Program.cs
+++ blazor_wasm/bingo_frontend_wasm/Program.cs
@@ -6,23 +6,23 @@ using System.Reflection;
 
 class Program
 {
+    //Globally accessible vars
     internal static string VERSIONNAME { get; set; } = "ms1-proto";
-    internal static string GITHASH { get; set; } = "";
-    internal static string COMMITDATE { get; set; } = "";
+    internal static string? GITHASH { get; set; } = string.Empty;
+    internal static string? COMMITDATE { get; set; } = string.Empty;
 
-    //TODO: Add dev environment flag string
+    internal static ENVIRONMENT_FLAG ENVIRONMENT { get; set; }
 
     static async Task Main(string[] args)
     {
-        GITHASH = Assembly.GetEntryAssembly()
+        GITHASH = Assembly.GetEntryAssembly()?
                           .GetCustomAttributes<AssemblyMetadataAttribute>()
                           .FirstOrDefault(attr => attr.Key == "GitHash")?.Value;
 
-        COMMITDATE = Assembly.GetEntryAssembly()
+        COMMITDATE = Assembly.GetEntryAssembly()?
                           .GetCustomAttributes<AssemblyMetadataAttribute>()
                           .FirstOrDefault(attr => attr.Key == "CommitDate")?.Value;
 
-        Console.WriteLine(GITHASH);
 
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
@@ -30,7 +30,23 @@ class Program
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+        if (builder.HostEnvironment.IsDevelopment())
+        {
+            ENVIRONMENT = ENVIRONMENT_FLAG.DEVELOPMENT;
+            Console.WriteLine($"Development version {VERSIONNAME} {GITHASH} built {COMMITDATE}");
+        }
+        else
+        {
+            ENVIRONMENT = ENVIRONMENT_FLAG.PRODUCTION;
+            Console.WriteLine($"Production version {VERSIONNAME} {GITHASH} built {COMMITDATE}");
+        }
 
         await builder.Build().RunAsync();
     }
+}
+
+enum ENVIRONMENT_FLAG
+{
+    DEVELOPMENT,
+    PRODUCTION
 }
