633b47e3daf784f8b3ed4a8a2f1623d4bb78863d
Vin <104235331+VinFG1@users.noreply.github.com>
2024-03-04 20:55:54 -0800

    Clean up, change URL

--- blazor_wasm/bingo_frontend_wasm/Program.cs
+++ blazor_wasm/bingo_frontend_wasm/Program.cs
@@ -30,7 +30,7 @@ class Program
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080") });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://bingo.juliapixel.com/") });//new Uri("http://localhost:8080") });
 
 
         if (builder.HostEnvironment.IsDevelopment())
