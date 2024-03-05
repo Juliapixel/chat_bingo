0d43b4a63fe731b47999e1d44f1c7a8447fb1516
Vin <104235331+VinFG1@users.noreply.github.com>
2024-03-04 20:13:08 -0800

    Add HTTPClient service, API test page, and service injection

--- blazor_wasm/bingo_frontend_wasm/Program.cs
+++ blazor_wasm/bingo_frontend_wasm/Program.cs
@@ -30,6 +30,9 @@ class Program
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080") });
+
+
         if (builder.HostEnvironment.IsDevelopment())
         {
             ENVIRONMENT = ENVIRONMENT_FLAG.DEVELOPMENT;
